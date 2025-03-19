32a7f0ae979c83e213b8f5933a728b5f1449ef2e
zainab <reemansur35@gmail.com>
2025-03-19 12:48:58 +0100

    my first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonApp, IonContent, IonFooter, IonHeader, IonRouterOutlet, IonTabBar, IonTitle, IonToolbar, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
 import Home from './pages/Home';
 
@@ -20,7 +20,7 @@ import '@ionic/react/css/flex-utils.css';
 import '@ionic/react/css/display.css';
 
 /**
- * Ionic Dark Mode
+ * Ionic dark Mode
  * -----------------------------------------------------
  * For more info, please see:
  * https://ionicframework.com/docs/theming/dark-mode
@@ -37,16 +37,22 @@ setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    <IonReactRouter>
-      <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
-        <Route exact path="/">
-          <Redirect to="/home" />
-        </Route>
-      </IonRouterOutlet>
-    </IonReactRouter>
+    <IonHeader>
+      <IonToolbar color="primary">
+        <IonTitle>
+          Wellcome, Chindo Zainab Abubakar
+        </IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent className="custom-content">
+      <h1 style={{textAlign:"center", paddingTop: "50px",}}>Welcome To Bootcamp</h1>
+    </IonContent>
+    <IonFooter>
+      <IonToolbar>
+        <IonTitle className="footer-title">   Day 1 Assignment
+        </IonTitle>
+      </IonToolbar>
+    </IonFooter>
   </IonApp>
 );
 
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonFooter } from '@ionic/react';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
+    
       <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+        <IonToolbar color="primary">
+          <IonTitle>Welcome, Zainab Chindo Abubakar </IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+
+      
+      <IonContent className="custom-content">
+        <h1>Welcome to Bootcamp</h1>
       </IonContent>
+
+      
+      <IonFooter>
+        <IonToolbar>
+          <IonTitle className="footer-text">Day 1 Assignment</IonTitle>
+        </IonToolbar>
+      </IonFooter>
     </IonPage>
   );
 };
+
 
 export default Home;
